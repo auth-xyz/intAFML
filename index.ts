bbccc71e7f1f8f3262bbd907f2f769b78dcba3da
Auth <authenticover@gmail.com>
2023-02-10 23:31:38 -0400

    god kill me

--- index.ts
+++ index.ts
@@ -1,84 +1,118 @@
-import fs from 'fs';
+// Import necessary modules
+import fs from "fs";
 
-interface Config {
-  [key: string]: any;
+// Define the supported types in AFML
+enum AFMLTypes {
+  String = "String",
+  Secret = "Secret",
+  Number = "Number",
+  Boolean = "Boolean",
+  JSON = "JSON",
+  Array = "Array",
 }
 
-enum AFMLDataType {
-  String = 'String',
-  Boolean = 'Boolean',
-  Number = 'Number',
-  Secret = 'Secret',
-}
+const parseValues = (value: string, type: any, allowSecret = true): any => {
+  switch (type) {
+    case AFMLTypes.String:
+      return value;
+    case AFMLTypes.Secret:
+      return allowSecret ? value : "*".repeat(value.length);
+    case AFMLTypes.Number:
+      return parseFloat(value);
+    case AFMLTypes.Boolean:
+      return value === "true";
+    case AFMLTypes.JSON:
+      return JSON.parse(value);
+    case AFMLTypes.Array:
+      return value.split(",").map((item) => item.trim());
+    default:
+      throw new Error(`[AFML] :: Unsupported type annotation: ${type}`);
+  }
+};
 
-function parseAFML(filePath: string, options: { allowSecret: boolean } = { allowSecret: false }): Config {
-  let config: Config = {};
-  let lines = fs.readFileSync(filePath, 'utf-8').split('\n');
+const parseAFML = (
+  filePath: string,
+  options: { allowSecret: boolean } = { allowSecret: false }
+) => {
+  // Read the content of the file
+  const fileContent = fs.readFileSync(filePath, "utf-8");
 
-  let currentSection = '';
+  // Split the content by line
+  const lines = fileContent.split("\n");
 
-  lines.forEach(line => {
-    line = line.trim();
+  // Keep track of the current section and its size limit
+  let currentSection: string | null = null;
+  let currentSectionSizeLimit: number | null = null;
 
-    if (line.startsWith('#') || line === '') return;
+  // Store the variables and their values
+  const variables: { [key: string]: any } = {};
 
-    let matchSection = line.match(/^\[(.*)\]$/);
-    if (matchSection) {
-      currentSection = matchSection[1];
-      config[currentSection] = {};
-      return;
+  // Parse each line
+  for (const line of lines) {
+    // Trim the line and remove any comments
+    const trimmedLine = line.trim().split("#")[0];
+
+    // Skip empty lines
+    if (!trimmedLine) {
+      continue;
     }
 
-    let parts = line.split(/\s*;\s*/);
-    let valueWithType = parts[0].trim();
-    let dataType = parts[1]?.trim();
+    // Check if the line starts a new section
+    if (trimmedLine.startsWith("[")) {
+      // Extract the section name and size limit
+      const sectionParts = trimmedLine
+        .slice(1, -1)
+        .split("(")
+        .map((part) => part.trim());
+      const sectionName = sectionParts[0];
+      const sectionSizeLimit = sectionParts[1]
+        ? parseInt(sectionParts[1].slice(0, -1))
+        : null;
 
-    if (!valueWithType) {
-      throw new Error(`Line '${line}' does not contain a value`);
+      // Update the current section and size limit
+      currentSection = sectionName;
+      currentSectionSizeLimit = sectionSizeLimit;
+
+      // Initialize the section in the variables object
+      variables[sectionName] = {};
+
+      continue;
     }
 
-    if (!AFMLDataType[dataType as keyof typeof AFMLDataType]) {
-      throw new Error(`Line '${line}' contains an invalid data type: ${dataType}`);
-    }
-
-    let valueAndKey = valueWithType.split(':');
-    let key = valueAndKey[0].trim();
-    let value: any = valueAndKey[1].trim();
-
-    if (dataType === AFMLDataType.Secret) {
-      if (!options.allowSecret) {
-        console.log(`Secret variable detected: ${key}`);
-        value = "****";
+    // Check if the line defines a variable
+    if (currentSection) {
+      // Check if the size limit of the current section has been reached
+      if (
+        currentSectionSizeLimit &&
+        Object.keys(variables[currentSection]).length >= currentSectionSizeLimit
+      ) {
+        throw new Error(
+          `[AFML] :: Section :"${currentSection}" went above set limit: ${currentSectionSizeLimit}`
+        );
       }
-    } else {
-      if (dataType === AFMLDataType.String) {
-        let matchString = value.match(/^"(.*)"$/);
-        if (matchString) {
-          value = matchString[1];
-        } else {
-          throw new Error(`Line '${line}' does not contain a valid string value`);
-        }
-      } else if (dataType === AFMLDataType.Boolean) {
-        if (value === 'true' || value === 'false') {
-          value = value === 'true';
-        } else {
-          throw new Error(`Line '${line}' does not contain a valid boolean value`);
-        }
-      } else if (dataType === AFMLDataType.Number) {
-        if (!isNaN(value)) {
-          value = Number(value);
-        } else {
-          throw new Error(`Line '${line}' does not contain a valid number value`);
-        }
+
+      // Extract the variable and its value
+      const parts = trimmedLine.split(":").map((part) => part.trim());
+      const variable = parts[0];
+      const value = parts[1].split(" *")[0].trim();
+      const type = parts[1].split(" *")[1]
+        ? (parts[1].split(" *")[1].trim() as AFMLTypes)
+        : AFMLTypes.String;
+
+      // Parse the value based on its type
+      let parsedValue: any;
+      if (options.allowSecret || type !== AFMLTypes.Secret) {
+        parsedValue = parseValues(value, type);
+      } else {
+        parsedValue = "*".repeat(value.length);
       }
+
+      // Assign the parsed value to the variable in the current section
+      variables[currentSection][variable] = parsedValue;
     }
+  }
 
-    config[currentSection][key] = value;
-  });
+  return variables;
+};
 
-  return config;
-}
-
-export { 
-  parseAFML
-}
+console.log(parseAFML("./config.afml"));
