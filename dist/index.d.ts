546360cbbfff743676207d5287af54915d1bac49
Auth <authenticover@gmail.com>
2023-02-12 01:39:28 -0400

    god just kill me already

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,6 @@
-export {};
+declare const parseAFML: (filePath: string, basePath?: string, options?: {
+    allowSecret: boolean;
+}) => {
+    [key: string]: any;
+};
+export { parseAFML };
--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.parseAFML = void 0;
 // Import necessary modules
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
@@ -122,4 +123,4 @@ const parseAFML = (filePath, basePath = process.cwd(), options = { allowSecret: 
     }
     return variables;
 };
-console.log(parseAFML("./config.afml"));
+exports.parseAFML = parseAFML;
